fbb12519878783cb2431112e84cd8d92dbd711fe
KARDxHIDDEN <jj.jade27@gmail.com>
2021-08-05 12:11:30 -0600

    nuevo procedimiento

---
+++ CAPA_DATOS/SeguimientoD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CAPA_DATOS
+{
+     public class SeguimientoD
+    {
+        string ID_Seguimiento;
+        string ID_Consulta;
+        DateTime Fecha_inicio;
+        DateTime Fecha_final;
+        string Observaciones;
+
+    }
+}
