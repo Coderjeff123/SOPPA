4a43a8441364868cfd87a795cc9b4acec7e3b213
KARDxHIDDEN <jj.jade27@gmail.com>
2021-10-06 09:27:46 -0600

    informe ejemplo

---
+++ CAPA_PRESENTACION/informe.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CAPA_PRESENTACION
+{
+    public partial class informe : Form
+    {
+        public informe()
+        {
+            InitializeComponent();
+        }
+
+        private void informe_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void reportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
